7a57979209ce3d82b273f1286a75dd63b975b1f1
Toshin Prince <toshinprince@gmail.com>
2023-09-29 16:37:46 +0530

    refactoring_code_for_priceFeed

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -8,12 +8,14 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 //A library is embedded into the contract if all library functions are internal.
 //Otherwise the library must be deployed and then linked before the contract is deployed.
 library PriceConverter {
-    function getPrice() internal view returns (uint256) {
-        //ABI
-        // ADDRESS(ETH/USD -Goreli) - 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
+    function getPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        // //ABI
+        // // ADDRESS(ETH/USD -Goreli) - 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(
+        //     0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        // );
         (, int256 price, , , ) = priceFeed.latestRoundData();
         //ETH in terms of USD
         //3000.00000000 - 8 Decimals
@@ -21,17 +23,19 @@ library PriceConverter {
         return uint256(price * 1e10); //1**10 == 10000000000
     }
 
-    function getVersion() internal view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
-        return priceFeed.version();
-    }
+    // //Don't need this function for this particular contract, which we are using this Library for
+    // function getVersion() internal view returns (uint256) {
+    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(
+    //         0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+    //     );
+    //     return priceFeed.version();
+    // }
 
     function getConversionRate(
-        uint256 ethAmount
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
-        uint256 ethPrice = getPrice();
+        uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
     }
