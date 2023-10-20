c2be2f3b4b9f320a11b0305b996dcd58a9c7b47e
Toshin Prince <toshinprince@gmail.com>
2023-10-20 16:21:29 +0530

    fun_Contract_variables

--- contracts/FunWithStorage.sol
+++ contracts/FunWithStorage.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract FunWithStorage {}
+contract FunWithStorage {
+    uint256 favoriteNumber;
+    bool someBool;
+    uint256[] myArray;
+}
